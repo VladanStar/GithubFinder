bee93ae09f9c0580b6715fac4f56de09c0929a81
VladanStar <vladancupric@gmail.com>
2022-09-07 06:22:46 +0200

    Add photo

--- src/components/page/Home/Home.js
+++ src/components/page/Home/Home.js
@@ -7,10 +7,23 @@ function Home() {
   <div className='search-form' >
     <h4>GitHub Search Users</h4>
     <form>
-      
+      <input type="text" />
+      <button>Search</button>
     </form>
   </div>
   <div className='search-results'>
+    <div className='user'>
+      <div className='image'>
+
+      </div>
+      <div className='user=info'>
+        <h4>Name of the User</h4>
+        <small>ID34fj</small>
+        <a href='http://cdn.vox-cdn.com/uploads/chorus_asset/file/22429121/imgres.0.html' alt="Photo user">View profile</a>
+
+      </div>
+
+    </div>
 
   </div>
 
