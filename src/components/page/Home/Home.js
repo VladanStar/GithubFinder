9d06f8b4530c7f1dafacb2669221da6706ea4e50
VladanStar <vladancupric@gmail.com>
2022-09-07 07:47:31 +0200

    Add user and home

--- src/components/page/Home/Home.js
+++ src/components/page/Home/Home.js
@@ -1,37 +1,106 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import "./Home.css";
+import axios from "../../../axios";
+import User from "../../ui/User";
+const Home = () => {
+  const [query, setQuery] = useState("");
+  //Users fetched from the API
+  const [users, setUsers] = useState([]);
+  //Page
+  const [page, setPage] = useState(1);
+  //Per page
+  const [limit, setLimit] = useState(10);
 
-function Home() {
+  const handleQueryInput = (e) => {
+    const value = e.target.value;
+    setQuery(value);
+  };
+  const handlePrevPage = () => {
+    setPage((page) => {
+      if (page === 1) return page;
+      else return page - 1;
+    });
+  };
+
+  const handleNextPage = () => {
+    setPage((page) => page + 1);
+  };
+
+  const handlePageLimit = (e) => {
+    const value = e.target.value;
+    setLimit(parseInt(value));
+  };
+
+  const fetchUsers = async () => {
+    try {
+      const { data } = await axios.get("/search/users?q=" + query, {
+        params: {
+          page,
+          per_page: limit,
+        },
+      });
+      return data?.items;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  const handleSearchUsers = async (e) => {
+    e.preventDefault();
+    if (query) {
+      const items = await fetchUsers();
+      setUsers(items);
+    } else {
+      console.log("Your query is empty...");
+    }
+  };
+
+  useEffect(() => {
+    const displayUsersOnChange = async () => {
+      if (query) {
+        const items = await fetchUsers();
+        setUsers(items);
+      }
+    };
+    displayUsersOnChange();
+  }, [page, limit]);
+
   return (
-    <div className='container'>
-  <div className='search-form' >
-    <h4>GitHub Search Users</h4>
-    <form>
-      <input type="text" />
-      <button>Search</button>
-    </form>
-  </div>
-  <div className='search-results'>
-    <div className='user'>
-      <div className='image'>
-        <img  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqZSEyJpE_EGC4XRRgLwnTs5XOUC_ZUelnxeQQL-YR92_1d8W0GEIBleZ62vr4cymSZ0Q&usqp=CAU'></img>
-
+    <div className="container">
+      <div className="search-form">
+        <h2>GitHub Search User</h2>
+        <form>
+          <input value={query} onChange={handleQueryInput} type="text" />
+          <button onClick={handleSearchUsers}>Search</button>
+        </form>
       </div>
-      <div className='user=info'>
-        <h4>Name of the User</h4>
-        <small>ID34fj</small>
-        <a href='http://cdn.vox-cdn.com/uploads/chorus_asset/file/22429121/imgres.0.html' alt="Photo user">View profile</a>
-
+      <div className="search-results">
+        <div className="more-options">
+          <label>
+            <small>Per Page</small>
+            <select onChange={handlePageLimit}>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>
+          </label>
+          <div className="pagination">
+            <button onClick={handlePrevPage}>{page}</button>
+            <button onClick={handleNextPage}>{page + 1}</button>
+          </div>
+        </div>
+        {users ? (
+          users.map((user) => {
+            return <User user={user} key={user.id} />;
+          })
+        ) : (
+          <h2>There is nothing to display...</h2>
+        )}
       </div>
-
     </div>
+  );
+};
 
-  </div>
-
-     
-
-      </div>
-  )
-}
-
-export default Home
+export default Home;
