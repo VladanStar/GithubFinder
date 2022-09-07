6e173de55691691f188ef2be22db735a38924b1b
VladanStar <vladancupric@gmail.com>
2022-09-07 06:59:56 +0200

    Add home.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Home from './components/page/Home/Home';
 
 function App() {
   return (
     <div className="App">
      Welcome Vladan Cupric
+     <Home />
     </div>
   );
 }
